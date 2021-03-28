4c43051cf5886fbd95bbd87488adfea96373d8cf
Yura <mr.Shapovaloff3101@gmail.com>
2021-03-28 15:36:44 +0300

    Add filtering by other parameters

--- js/map.js
+++ js/map.js
@@ -1,8 +1,9 @@
 /* global L:readonly */
+/* global _:readonly */
 import { initForm } from './form.js';
 import { renderCard } from './render-card.js';
 import { getData } from './api.js';
-import {filterByType} from './filters.js';
+import { getFilterObject, getFilteredData } from './filters.js';
 
 const TOKYO_LATITUDE = 35.68950;
 const TOKYO_LONGITUDE = 139.69171;
@@ -12,9 +13,11 @@ const MAIN_PIN_WIDTH = 52;
 const MAIN_PIN_HEIGHT = 52;
 const PIN_WIDTH = 40;
 const PIN_HEIGHT = 40;
-const ANY_CHOICE_FILTERS = 'any';
 // Количество объявлений
 const ADS_COUNT = 10;
+
+// Задержка для _.debounce()
+const RERENDER_DELAY = 500;
 
 // Карта
 const map = L.map('map-canvas');
@@ -26,28 +29,22 @@ const adForm = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
 const inputAddress = adForm.querySelector('#address');
 
-// Фильтры
-const typeFilter = mapFilters.querySelector('#housing-type');
-
-// Переключение состояния страницы в зависимости от инициализации карты
+// Переключение состояния фильтров
 const toggleMapFiltersState = (isMapInit) => {
   for (let element of mapFilters.children) {
     element.disabled = !isMapInit;
   }
 
-  isMapInit
-    ? mapFilters.classList.remove('map__filters--disabled')
-    : mapFilters.classList.add('map__filters--disabled');
+  isMapInit ? mapFilters.classList.remove('map__filters--disabled') : mapFilters.classList.add('map__filters--disabled');
 };
+
 // Переключение состояния формы
 const toggleFormState = (isMapInit) => {
   for (let element of adForm.children) {
     element.disabled = !isMapInit;
   }
 
-  isMapInit
-    ? adForm.classList.remove('ad-form--disabled')
-    : adForm.classList.add('ad-form--disabled')
+  isMapInit ? adForm.classList.remove('ad-form--disabled') : adForm.classList.add('ad-form--disabled');
 };
 
 // Добавили слой карты
@@ -94,17 +91,17 @@ const renderMainMarker = () => {
   });
 };
 
-// Удалине всех маркеров с карты
+// Удаление всех маркеров с карты
 const removeAllMarkers = (markers) => {
   markers.forEach(value => value.remove())
 };
 
 // Создание обычных пинов
-const renderMarkers = (ads, type = ANY_CHOICE_FILTERS) => {
-  // Копируем, что бы не повредить исходные данные с сервера, фильтруем по типу
+const renderMarkers = (ads, filter) => {
+  // Копируем, чтобы не повредить исходные данные с сервера, фильтруем по типу
   const filteredAds = ads
     .slice()
-    .filter((value) => filterByType(value, type));
+    .filter((value) => getFilteredData(value, filter));
 
   if (markers.length) {
     removeAllMarkers(markers);
@@ -132,24 +129,37 @@ const renderMarkers = (ads, type = ANY_CHOICE_FILTERS) => {
           icon: pinIcon,
         });
 
-      marker.bindPopup(renderCard(value), {
-        keepInView: true,
-      });
+      marker.bindPopup(renderCard(value),
+        {
+          keepInView: true,
+        },
+      );
+
+      // Добавили в массив и на карту
       markers.push(marker);
       marker.addTo(map);
     });
 };
 
+const onFiltersChanged = _.debounce((ads, filterObject) => {
+  renderMarkers(ads, filterObject)
+}, RERENDER_DELAY);
+
 // Инициализация карты
 const initMap = () => {
   toggleMapFiltersState(false);
+  toggleFormState(false);
   map.on('load', () => {
     toggleFormState(true);
     initForm();
     getData()
       .then((ads) => {
-        renderMarkers(ads.slice(0, ADS_COUNT));
-        typeFilter.addEventListener('change', (evt) => renderMarkers(ads, evt.target.value));
+        const filterObject = getFilterObject();
+        renderMarkers(ads, filterObject);
+        mapFilters.addEventListener('change', (evt) => {
+          const filterObject = getFilterObject(evt);
+          onFiltersChanged(ads, filterObject);
+        });
       })
       .then(() => toggleMapFiltersState(true));
   })
